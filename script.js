f2e90770059dd74b2e32062f9cea0789097057a6
MRAx09 <martin.rae95@gmail.com>
2021-04-18 13:55:48 -0700

    Code edits

---
+++ script.js
@@ -0,0 +1,31 @@
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 10;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till quiz ends.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+    console.log("Time's UP!");
+
+}
+
+setTime();
